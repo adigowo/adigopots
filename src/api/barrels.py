bbf309d459b00b979f9a4b11dfd78b7b85f57776
adigowo <94579818+adigowo@users.noreply.github.com>
2024-04-15 18:45:08 -0700

    a
    a

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
-from sqlalchemy import text
+import sqlalchemy
 from src.database import engine  
+
+
 
 router = APIRouter(
     prefix="/barrels",
@@ -21,6 +23,18 @@ class Barrel(BaseModel):
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     """Endpoint for delivering barrels."""
     print(f"barrels delivered: {barrels_delivered} order_id: {order_id}")
+    with engine.begin() as connection:  
+        result = connection.execute(sqlalchemy.text("SELECT num_green_ml,num_green_potions,gold FROM global_inventory"))
+        row = result.fetchone()
+        num_green_ml = row[0]
+        num_green_potions = row[1]
+        gold = row[2]
+        for barrel in barrels_delivered:
+            gold = gold - barrel.price
+            num_green_ml = num_green_ml + barrel.ml_per_barrel
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = :gold"), {"gold": gold})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = :green_ml"), {"green_ml": num_green_ml})
+
     return "OK"
 
 # Gets called once a day
@@ -29,27 +43,35 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """Determines which barrels to purchase based on inventory and gold."""
     purchase_plan = []
     # SQL query 
-    select_sql = "SELECT num_green_potions, gold FROM global_inventory WHERE id=1;"
+    
     with engine.begin() as connection:  
-        result = connection.execute(text(select_sql)).fetchone()
-        if result:
-            num_green_potions, gold = result
+        result = connection.execute(sqlalchemy.text("SELECT num_green_ml,num_green_potions,gold FROM global_inventory"))
+        row = result.fetchone()
+        num_green_ml = row[0]
+        num_green_potions = row[1]
+        gold = row[2]
             
-            if num_green_potions < 10 and gold >= 50:
-                for barrel in wholesale_catalog:
-                    if barrel.sku == "SMALL_GREEN_BARREL" and barrel.price <= gold:
-                        quantity_to_buy = gold // barrel.price
-                        purchase_plan.append({
+        if num_green_potions < 10 and gold >= 50:
+            for barrel in wholesale_catalog:
+                if barrel.potion_type == [0,100,0,0] and barrel.price <= gold:
+                    quantity_to_buy = gold // barrel.price
+                    purchase_plan.append({
                             "sku": barrel.sku,
                             "quantity": quantity_to_buy,
-                        })
-                       
-                        update_sql = """
-                        UPDATE global_inventory
-                        SET gold = gold - (:price * :quantity),
-                            num_green_ml = num_green_ml + (:ml_per_barrel * :quantity)
-                        WHERE id=1;
-                        """
-                        connection.execute(text(update_sql), price=barrel.price, quantity=quantity_to_buy, ml_per_barrel=barrel.ml_per_barrel)
-                        break
-    return purchase_plan
+                    })
+                if barrel.potion_type == [100,0,0] and barrel.price <= gold:
+                    quantity_to_buy = gold // barrel.price
+                    purchase_plan.append({
+                            "sku": barrel.sku,
+                            "quantity": quantity_to_buy,
+                    })
+                if barrel.potion_type == [0,0,100,0] and barrel.price <= gold:
+                    quantity_to_buy = gold // barrel.price
+                    purchase_plan.append({
+                            "sku": barrel.sku,
+                            "quantity": quantity_to_buy,
+                    })
+                
+                      ##  connection.execute(text(update_sql), price=barrel.price, quantity=quantity_to_buy, ml_per_barrel=barrel.ml_per_barrel)
+
+        return purchase_plan
